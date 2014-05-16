e30e5559e00bdb2efa235f86c302a1719a619b8e
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-16 18:20:49 +0200

    fertig: generatePopulation
    initiale Mutationsrate als Parameter hinzugefügt. Ist am Anfang für alle
    Gene gleich. Wird vor der ersten Mutation ja sowieso fürjedes Gen
    einzeln nochmal durch Adaption verändert

--- Blatt02/generatePopulation.m
+++ Blatt02/generatePopulation.m
@@ -1,18 +1,24 @@
-function population = generatePopulation(numIndivids, numGenes, minVals, maxVals)
+function population = generatePopulation(numIndivids, numGenes, minVals, maxVals, initMutationRate)
 %GENERATEPOPULATION
 % Erzeugt eine Population in einer Matrix. Jede Zeile der Matrix
 % repreaesentiert das Genom eines Individuums. Hinter den Genen werden die
 % Mutationsraten der einzelnen Gene hinterlegt.
 % Jedes Gen ist eine Zufallszahl im Interval [minVal, maxVal] 
 % Parameter:
-%   numIndivids:    Anzahl der Individuen
-%   numGenes:       Anzahl der Gene und MutationsRaten
-%   minVals:        Array fuer unteren Grenzen des Intervals der Gene
-%   maxVals:        Array fuer oberen Grenzen des Intervals der Gene
+%   numIndivids:        Anzahl der Individuen
+%   numGenes:           Anzahl der Gene und MutationsRaten
+%   minVals:            Array fuer unteren Grenzen des Intervals der Gene
+%   maxVals:            Array fuer oberen Grenzen des Intervals der Gene
+%   initiMutationRate:  Initiale Mutationsrate aller Gene
 
 population = zeros(numIndivids,2*numGenes);
+
+% initiale Mutationsrate hinter die Gene schreiben
+population(:,numGenes:end) = initMutationRate;
+
+% Gene mit Zufallszahlen in den gegebenen Intervallen initialisieren
 for i=1:size(population,1)
-    population(i,:) = (maxVals-minVals).*rand(1,numGenes) + minVals;
+    population(i,1:numGenes) = (maxVals-minVals).*rand(1,numGenes) + minVals;
 end
 
 end
