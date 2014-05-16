75282b36b99ad0fe5def3aabfe1a0b8c21e9a306
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-16 14:34:24 +0200

    ok

--- Blatt01/1_2/getRepresentative.m
+++ Blatt01/1_2/getRepresentative.m
@@ -1,4 +1,4 @@
-function [representativeBoard, backTrans] = getRepresentative(boardSituation)
+function [representativeBoard, backTrans] = getRepresentative(boardSituation, equivalentClasses)
 %GETREPRESENTATIVE
 % Bildet eine SpielfeldSituation durch Rotation und Spiegelung auf seine
 % Aequivalenzklasse ab. Gibt diese als Matrix/SpielfeldSituation zurueck
@@ -7,31 +7,12 @@ function [representativeBoard, backTrans] = getRepresentative(boardSituation)
 % Eine Rotation mit 'rot90()' wird in Matlab immer gegeb den Uhrzeigersinn 
 % ausgefuehrt
 
-% Transformationen:
+% Alle Transformationen berechnen
+trafoMat = calculateTrafos(boardSituation);
 
-% Rotation um 90 Grad
-trafoMat{1,1} = rot90(boardSituation,1);
+% nach passender Aequivalenzklasse suchen
 
-% Rotation um 180 Grad
-trafoMat{2,1} = rot90(boardSituation,2);
 
-% Rotation um 270 Grad
-trafoMat{3,1} = rot90(boardSituation,3);
-
-% Rotation um 90 Grad + Spiegelung an x-Achse
-trafoMat{4,1} = flipud(trafoMat{1,1});
-
-% Rotation um 90 Grad + Spiegelung an y-Achse
-trafoMat{5,1} = fliplr(trafoMat{1,1});
-
-% Rotation um 180 Grad + Spiegelung an x-Achse
-trafoMat{6,1} = flipud(trafoMat{2,1});
-
-% Rotation um 180 Grad + Spiegelung an y-Achse
-trafoMat{7,1} = fliplr(trafoMat{2,1})
-
-representativeBoard = trafoMat;
-backTrans = 0;
 
 end
 
