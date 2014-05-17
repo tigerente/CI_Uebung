5fa3d15a0704c6d752234669fef316ebb715b835
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-17 18:38:58 +0200

    reproduce angefangen und fertig
    Noch nicht getestet bis jetzt, müsste aber passen.
    Mögliches Problem: Sichtbarkeit der VAriablen 'alpha' und 'parentidx1'
    bzw 'parentIdx2'. Diese müssten evtl vor der großen For-Schleife
    indiziert werden...

---
+++ Blatt02/reproduce.m
@@ -0,0 +1,80 @@
+function newPopulation = reproduce(parentsPool, lambda, flagGlobalRekombination, flagDiscreteOrArithmetic, flagAlphaGlobal, flagLambdaPlusMy)
+%REPRODUCE 
+% Funktion zur Erzeugnung einer Kindergeneration aus den gewaehlten Eltern.
+% Es koennen alle Arten der Rekombination mittels Flag-Variablen
+% ausgewaehlt werden
+% Parameter:
+%   parentsPool:                ElternPool mit ausgewaehlten Elterngenomen
+%   lambda:                     Anzahl der zu generierenden Kinder
+%   flagGlobalRekombination:    Soll globale Rekombination verwendet
+%                               werden? (fuer jedes Gen zwei zufaellige Eltern)
+%   flagDiscreteOrArithmetic:   true => diskrete Rekombination
+%                               false => arithmetische Rekombination
+%   flagAlphaGlobal:            true => arithmetischer Operator 'alpha' wird fuer
+%                               ganzes Genom gewaehlt
+%                               false => 'alpha' wird fuer jedes Gen einzeln gewaehlt
+%   flagLambdaPlusMy:           true => (Lambda + My)
+%                               false => (Lambda,My)
+
+% Anzahl Gene
+numGenes = size(parentsPool,2);
+
+% Zwischenspeicherung der 'lambda' Nachkommen, aus denen spaeter die 'my'
+% besten gewaehlt werden (bei (lambda,my)-Kriterium)
+lambdaMatrix = zeros(lambda,numGenes);
+
+% Rekombination fuer jedes Kind durchfuehren
+for i=1:lambda
+
+    
+    % Wenn keine globale Rekombination
+    if flagGlobalRekombination == false
+        
+        % Zufaellige Eltern fuer ganzes Genom waehlen
+        parentIdx1 = randi([1, size(parentsPool,1)]);
+        parentIdx2 = randi([1, size(parentsPool,1)]);
+    end
+    
+    % Wenn gleiches 'alpha' fuer ale Gene
+    if flagAlphaGlobal == true
+        alpha = rand;
+    end
+    
+    % Durch jedes Gen gehen
+    for g=1:numGenes
+        
+        % Wenn Globale Rekombination
+        if flagGlobalRekombination == true
+            % Zufaellige Eltern fuer Gen waehlen
+            parentIdx1 = randi([1, size(parentsPool,1)]);
+            parentIdx2 = randi([1, size(parentsPool,1)]);
+        end
+                
+        % Wenn diskret rekombiniert werden soll
+        if flagDiscreteOrArithmetic == true
+            
+            % Auswaehlen, welches Elternteil fuer jeweiliges Gen
+            if rand < 0.5
+                lambdaMatrix(i,g) = parentsPool(parentIdx1,g);
+            else
+                lambdaMatrix(i,g) = parentsPool(parentIdx2,g);
+            end
+        
+        % Wenn arithmetisch rekombiniert werden soll
+        elseif flagDiscreteOrArithmetic == false
+            
+            % Wenn fuer jedes Gen ein 'alpha' benoetigt
+            if flagAlphaGlobal == false
+               alpha = rand;
+            end
+            
+            % arithmetisch rekombinieren
+            lambdaMatrix(i,g) = alpha*parentsPool(parentIdx1,g) + (1-alpha)*parentsPool(parentIdx2,g);
+        end
+        
+    end
+        
+end
+
+end
+
