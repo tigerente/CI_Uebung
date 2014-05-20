311c87c23c1617d16727adc98e2339c1df75625b
tigerente <janos.sebestyen@web.de>
2014-05-20 08:35:16 +0200

    Methode generatePopulation ueberprueft

--- Blatt02/generatePopulation.m
+++ Blatt02/generatePopulation.m
@@ -1,25 +1,25 @@
 function population = generatePopulation(numIndivids, numGenes, minVals, maxVals, initMutationRate)
 %GENERATEPOPULATION
 % Erzeugt eine Population in einer Matrix. Jede Zeile der Matrix
-% repreaesentiert das Genom eines Individuums. Hinter den Genen werden die
-% Mutationsraten der einzelnen Gene hinterlegt.
-% Jedes Gen ist eine Zufallszahl im Interval [minVal, maxVal] 
+% repreaesentiert das Genom eines Individuums. Hinter den Genen werden in
+% gleicher Reihenfolge die Mutationsraten der einzelnen Gene hinterlegt.
+% Jedes Gen ist eine Zufallszahl im Interval [minVals, maxVals] 
 % Parameter:
 %   numIndivids:        Anzahl der Individuen
-%   numGenes:           Anzahl der Gene und MutationsRaten
+%   numGenes:           Anzahl der Gene (=Anzahl der Mutationsraten)
 %   minVals:            Array fuer unteren Grenzen des Intervals der Gene
 %   maxVals:            Array fuer oberen Grenzen des Intervals der Gene
 %   initiMutationRate:  Initiale Mutationsrate aller Gene
 
 population = zeros(numIndivids,2*numGenes);
 
-% initiale Mutationsrate hinter die Gene schreiben
-population(:,numGenes:end) = initMutationRate;
-
 % Gene mit Zufallszahlen in den gegebenen Intervallen initialisieren
-for i=1:size(population,1)
+for i=1:numIndivids
     population(i,1:numGenes) = (maxVals-minVals).*rand(1,numGenes) + minVals;
 end
+
+% initiale Mutationsrate hinter die Gene schreiben
+population(:,numGenes:end) = initMutationRate;
 
 end
 
