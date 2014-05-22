94c9ed42cb88feb21e91d25fb572b46f20c14bf6
tigerente <janos.sebestyen@web.de>
2014-05-22 15:40:12 +0200

    treeMutate: rekursiver Versuch
    geht auch nicht, weil unklar, wie Blaetter erkannt werden sollen

--- Blatt03/testTreeMutate.m
+++ Blatt03/testTreeMutate.m
@@ -1,2 +1,4 @@
 tree = treeRandGenDeep(4, 0.6, 4, 9);
-treeMutate(tree, 0.3, 3, 0.6, 4, 9);
+treeShow(tree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+mutatedTree = treeMutate(tree, 0.3, 3, 0.6, 4, 9);
+treeShow(mutatedTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
