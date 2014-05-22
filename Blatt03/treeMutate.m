7cd06054b8ff81fe55d0d1c0f4506040885b25f2
tigerente <janos.sebestyen@web.de>
2014-05-22 17:32:20 +0200

    treeMutate: Aufräumarbeiten
    Ausgaben und ueberfluessigen Code entfernt

--- Blatt03/treeMutate.m
+++ Blatt03/treeMutate.m
@@ -16,8 +16,6 @@ function mutatedTree = treeMutate(tree,mutateProb,maxMutateDepth,descprobab, nrO
 mutated = zeros(size(tree,1),1); % ggf. wird der Platz dynamisch erweitert
 mutatedTree = tree;
 
-treeShow(tree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
-
 % Gehe alle Knoten b des mutierten Bauemes durch
 % Bemerkung: Der Baum aendert waehrend des Durchlaufs ggf. seine Groesse
 % und Struktur. Dadurch aendert sich auch fortlaufend die Knotennummerierung
@@ -28,35 +26,37 @@ treeShow(tree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
 
 b = 1;
 while b < size (mutatedTree, 1)
+    
     parentNode = mutatedTree(b,2);
+    
     % Falls Elternknoten von b noch nicht mutiert wurde...
     if (parentNode == 0) || ~(mutated(parentNode))
         % Mit Wahrscheinlichkeit mutateProb...
         if (rand < mutateProb)
-            display (['Mutiere Knoten '  num2str(b)]);
+
             % entferne b und seinen Teilbaum:
             [treeRemoved, left] = treeRemove(mutatedTree, b);
-            % figure;
-            % treeShow(treeRemoved, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+
             % erzeuge zufaelligen Baum:
             randTree = treeRandGenDeep(maxMutateDepth, descprobab, nrOp, nrTerm);
-            % figure;
-            % treeShow(randTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+
             % fuege zufaelligen Baum an die Stelle von b ein:
             mutatedTree = treeAppend(treeRemoved, parentNode, randTree, left);
+            
             % markiere b als mutiert:
             mutated(b)=1;
-            figure;
-            treeShow(mutatedTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+
         else
             mutated(b)=0;
         end
+        
     else if mutated(parentNode)
             % Da der Elternknoten mutiert wurde, ist auch der Kindknoten
-            % bereits verändert: gilt als mutiert.
+            % bereits verändert und gilt daher als mutiert.
             mutated(b)=1;
         end
     end
+    
     % weiter gehts:
     b = b + 1;
 end
