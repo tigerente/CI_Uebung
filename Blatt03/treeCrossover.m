df83b3312cf091f5847a5ade2a9b5d242d964f9b
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-22 12:15:00 +0200

    treeCrossover() angefangen

---
+++ Blatt03/treeCrossover.m
@@ -0,0 +1,13 @@
+function [child1 child2] = treeCrossover(parent1, parent2)
+%TREECROSSOVER(parent1, parent2)
+% Funktion zur Rekombination durch Austausch von Teilbaeumen
+% PARAMETER:
+%   parent1:    Elternbaum, der mit 'parent2' Nachkommen erzeugen soll
+%   parent2:    Elternbaum, der mit 'parent1' Nachkommen erzeugen soll
+% 
+% RETURN:
+%   child1:     Durch Crossover entstandenes Kind Nr. 1
+%   child2:     Durch Crossover entstandenes Kind Nr. 2
+
+end
+
