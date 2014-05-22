590a7b9c304d44de0003feb332b353b05815b209
tigerente <janos.sebestyen@web.de>
2014-05-22 15:40:35 +0200

    Revert "treeMutate: rekursiver Versuch"
    This reverts commit 94c9ed42cb88feb21e91d25fb572b46f20c14bf6.

--- Blatt03/treeMutate.m
+++ Blatt03/treeMutate.m
@@ -1,4 +1,4 @@
-function mutatedTree = treeMutate(tree, mutateProb, maxMutateDepth, descprobab, nrOp, nrTerm)
+function mutatedTree = treeMutate(tree,mutateProb,maxMutateDepth,descprobab, nrOp, nrTerm)
 %TREEMUTATE Fuehrt eine Reihe von Teilbaummutationen durch.
 %   Eingabe-Paremter:   
 %       - tree: der zu mutierende Baum als (m x 2)-Matrix, wobei m die
@@ -12,20 +12,35 @@ function mutatedTree = treeMutate(tree, mutateProb, maxMutateDepth, descprobab, 
 %   Rückgabe-Parameter:
 %       - mutatedTree: mutierter Baum
 
-figure;
-treeShow(tree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
-% Mit Wahrscheinlichkeit mutateProb...
-if (rand < mutateProb)
-    mutatedTree = treeRandGenDeep(maxMutateDepth, descprobab, nrOp, nrTerm);
-else
-    treeLeft = TreeGetSub(tree, 2);
-    treeRight = TreeGetSub(tree, 3);
-    treeLeftMutated = treeMutate(treeLeft, mutateProb, maxMutateDepth, descprobab, nrOp, nrTerm);
-    treeRightMutated = treeMutate(treeRight, mutateProb, maxMutateDepth, descprobab, nrOp, nrTerm);
-    root = tree(1,:);
-    mutatedTree = treeAppend(root,1,treeLeftMutated,1);
-    mutatedTree = treeAppend(mutatedTree,1,treeRightMutated,0);
-end
+% Initialisierung:
+mutated = zeros(size(tree,1),1);
+mutatedTree = tree;
 
+treeShow(tree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+
+% Gehe alle Knoten b durch:
+for b = 1 : size(tree,1)
+    parentNode = tree(b,2);
+    % Falls Elternknoten von b noch nicht mutiert wurde...
+    if (parentNode == 0) || ~(mutated(parentNode))
+        % Mit Wahrscheinlichkeit mutateProb...
+        if (rand < mutateProb)
+            disp (['Ersetze Knoten ' num2str(b)]);
+            % ersetze Teilbaum des Knotens b durch zufaelligen Teilbaum
+            [treeRemoved, left] = treeRemove(tree,b);
+            figure;
+            treeShow(treeRemoved, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+            randTree = treeRandGenDeep(maxMutateDepth, descprobab, nrOp, nrTerm);
+            figure;
+            treeShow(randTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+            mutatedTree = treeAppend(treeRemoved, parentNode, randTree, left);
+            % markiere b als mutiert
+            mutated(b)=1;
+            figure;
+            treeShow(mutatedTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+        end
+    end
+    
+end
 end
 
