bd2c2fb923976b357f368b98767327702f7901ec
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-22 12:10:20 +0200

    generateForest() fertig

--- Blatt03/generateForest.m
+++ Blatt03/generateForest.m
@@ -2,16 +2,25 @@ function forest = generateForest(numTrees, maxDepth, descProba, nrOp, nrTerm)
 %GENERATEFOREST(numTrees, macDepth, descProba, nrOp, nrTerm)
 % Erzeugt eine Population an Baeumen (Forest) mit Hilfe der Funktion
 % 'treeRandGenDeep'
-% Parameter: 
+% PARAMETER: 
 %   numTrees:   Anzahl der zu erzeugenden Baeume
 %   maxDepth:   Maximale Tiefe eines jeden Baumes
 %   descProba:  Abstiegswahrscheinlichkeit bei der Erzeugung eines Baums
 %               (wenn 1, immer vollstaendiger Baum)
 %   nrOp:       Groesse der Menge der Operatoren
 %   nrTerm:     Groesse der Menge der Terminalsymbole
+% 
+% RETURN:
+%   Den Forest als Cell-Array
 
+% Wald erzeugen
+forest = {13,1};
 
-
+% Forest mit Baeumen fuellen
+% (Evtl spaeter noch vektorisieren, wenn moeglich?!?!)
+for i=1:numTrees
+    forest{i} = treeRandGenDeep(maxDepth,descProba,nrOp,nrTerm);
+end
 
 end
 
