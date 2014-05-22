ec5bcdf4415f4b2e83540221170400869710099e
tigerente <janos.sebestyen@web.de>
2014-05-22 14:53:40 +0200

    treeMutate: kleine Korrektur
    schon besser, aber immer noch nicht korrekt

--- Blatt03/treeMutate.m
+++ Blatt03/treeMutate.m
@@ -16,27 +16,31 @@ function mutatedTree = treeMutate(tree,mutateProb,maxMutateDepth,descprobab, nrO
 mutated = zeros(size(tree,1),1);
 mutatedTree = tree;
 
-treeShow(tree, ['+' '-' '*' '/'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+treeShow(tree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
 
 % Gehe alle Knoten b durch:
 for b = 1 : size(tree,1)
-    % Falls Elternknoten von b noch nicht mutiert wurde...
     parentNode = tree(b,2);
+    % Falls Elternknoten von b noch nicht mutiert wurde...
     if (parentNode == 0) || ~(mutated(parentNode))
         % Mit Wahrscheinlichkeit mutateProb...
         if (rand < mutateProb)
             disp (['Ersetze Knoten ' num2str(b)]);
             % ersetze Teilbaum des Knotens b durch zufaelligen Teilbaum
-            [removedTree, left] = treeRemove(tree,b);
-            treeShow(removedTree, ['+' '-' '*' '/'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+            [treeRemoved, left] = treeRemove(tree,b);
+            figure;
+            treeShow(treeRemoved, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
             randTree = treeRandGenDeep(maxMutateDepth, descprobab, nrOp, nrTerm);
-            treeShow(randTree, ['+' '-' '*' '/'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
-            mutatedTree = treeAppend(mutatedTree, parentNode, randTree, left);
+            figure;
+            treeShow(randTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+            mutatedTree = treeAppend(treeRemoved, parentNode, randTree, left);
             % markiere b als mutiert
             mutated(b)=1;
-            treeShow(mutatedTree, ['+' '-' '*' '/'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
+            figure;
+            treeShow(mutatedTree, ['a' 'b' 'c' 'd'], ['1' '2' '3' '4' '5' '6' '7' '8' '9']);
         end
     end
-end    
+    
+end
 end
 
