2bba088b85a541b65a7821be6076e7a057729ef9
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-23 14:31:16 +0200

    treeNextGeneration() angefangen

---
+++ Blatt03/treeNextGeneration.m
@@ -0,0 +1,28 @@
+function newForest = treeNextGeneration(forest,fitness,mutateCrossoverProb,mutateProb,maxMutateDepth,descProb,nrOp,nrTerm)
+%TREENEXTGENERATION(forest, fitness, mutateCrossoverProb, mutateProb, maxMutateDepth, descProbab, nrOp, nrTerm)
+% Erstellt aus einem gegebenen Wald einen neuen Wald
+% Individuen werden entweder durch Mutation oder Rekombination erzeugt.
+% Selektion der Eltern fitnessproportional durch Statistica Universal
+% Sampling
+%PARAMETER:
+%   forest:                 Eltern-Wald/-Population
+%   fitness:                Vektor mit Fitnesswerten der einzelnen Individuen
+%   mutateCrossoverProb:    Wahrscheinlichkeit fuer Mutation bzw.
+%                           Gegenwahrscheinlichkeit fuer Rekombination. 
+%   mutateProb:             Wahrscheinlichkeit fuer Mutation eines Knotens
+%   maxMutateDepth:         Maximale Tiefe von mutierten Baeumen
+%   descProb:               Abstiegswahrscheinlichkeit bei Mutation
+%   nrOp:                   Anzahl der moeglichen Operatoren
+%   nrTerm:                 Anzahl der moeglichen Terminale
+%
+% RETURN:
+%   Einen neuen Wald
+
+
+
+
+
+
+
+end
+
