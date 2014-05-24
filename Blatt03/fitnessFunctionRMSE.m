fb5de97834d959e319f07f31b787847769528b08
FrankosLosTankos <frank.mehne@gmx.de>
2014-05-24 13:35:10 +0200

    Aufgabe 2a angefangen => Fitnessfunktion angefangen

---
+++ Blatt03/fitnessFunctionRMSE.m
@@ -0,0 +1,17 @@
+function fitness = fitnessFunctionRMSE(tree, data)
+%FITNESSFUNCTIONRMSE(tree, data)
+% Bestimmt die Fitness eines Baumes.
+% Zur Bestimmung der Fitness wird der RMSE der, von der Funktion des Baumes
+% erzeugten Daten und der uebergebenen Daten bestimmt
+% PARAMETER
+%   tree:       Baum, der bewertet werden soll
+%   data:       Paar aus Daten, mit dem verglichen werden soll
+% 
+% RETURN:
+%   fitness: Fitness des Baumes = 1/(1+RMSE)
+
+
+
+
+end
+
