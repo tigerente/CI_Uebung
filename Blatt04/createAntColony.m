2ee6fd95f75449b2140162215d048679a6e8f2ab
tigerente <janos.sebestyen@web.de>
2014-05-31 20:11:03 +0200

    Aufgabe 2: Ansatz von Frank

---
+++ Blatt04/createAntColony.m
@@ -0,0 +1,14 @@
+function antColony = createAntColony(nrOfIndivids)
+%CREATEANTCOLONY(nrOfIndivids)
+% Erstellt eine Ameisenkolonie
+% PARAMETERS:
+%   nrOfIndivids:       Anzahl der Ameisen
+%
+% RETURN:
+%   antColony:          Cell-Matrix mit 'nrOfIndivids' Zeilen. In jede Cell
+%                       wird spaeter der Pfad geschrieben, den die Ameise
+%                       laeuft
+
+antColony = cell(nrOfIndivids,1);
+
+end
---
+++ Blatt04/pickNextStreet.m
@@ -0,0 +1,27 @@
+function idxNextNode = pickStreet(pheromoneMap, currentNodeIdx)
+%PICKNEXTSTREET(pheromoneMap, currentNodeIdx)
+% Waehlt proportional zur Pheromonstaerke eine Kante des Graphen aus, der
+% durch 'pheromoneMap' repraesentiert wird
+% PARAMETER:
+%   pheromoneMap:       Graph, in dem statt Kantenkosten
+%                       Pheromonkonzentrationen stehen
+%   currentNodeIdx:     Index der aktuellen Position (aktueller Knoten)
+%
+% RETURN:       
+%   idxNextNode:        Index des ausgewehlten naechsten Knotens
+
+
+% Pheromonkonzentration der Kanten auslesen
+targetNodes = find(pheromoneMap(currentNodeIdx,:) > 0)';
+pheromones = pheromoneMap(targetNodes);
+pheromones(:,2) = targetNodes;
+
+% Pheromonkonzentrationen relativieren und sortieren
+pheromones(:,1) = pheromones(:,1)/sum(pheromones(:,1));
+pheromones = sortrows(pheromones,-1);
+
+% Naechsten Knoten je nach Pheromonkonzentration auswaehlen
+idx = find(cumsum(pheromones(:,1))>=rand);
+idxNextNode = pheromones(idx(1),2);
+
+end
