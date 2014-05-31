dc3853a6471a92c95b04fb766fb29f063f36d701
tigerente <janos.sebestyen@web.de>
2014-05-31 11:47:12 +0200

    Schönheitsarbeiten

--- Blatt04/PSO_a.m
+++ Blatt04/PSO_a.m
@@ -1,10 +1,8 @@
-plot = false;
+% 1) TESTPARAMETER
+flagPlot = true;
 nrRuns = 100;
-nIters = zeros (1, nrRuns);
-nFuncCalls = zeros (1, nrRuns);
 
-% _________________________________________________________________________
-%Parameter des PSO
+% 2) PARAMETER DES PSO
 nrParticle = 7^2; % Anzahl an Partikeln (Quadratzahl fuer regulaere Startverteilung)
 vmin = 0.0001; vmax = 12; % Minimale bzw. maximale Geschwindigkeit
 c0 = 0.3; % Traegheit
@@ -14,24 +12,20 @@ sPosRndElseRegl = true; % true: Startpositionen sind zufaellig gleichverteilt
                         % false: Startpositionen sind gleichmaeßig verteilt
 sVelRndElseZero = true; % true: Startgeschwindigkeiten sind zufaellig gleichverteilt
                         % false: Startgeschwindigkeiten = 0
-%__________________________________________________________________________
 
+% 3) KONSTANTEN
 %Bedeutung der einzelnen Eintraege
 X = 1; Y = 2; % Position
 DX = 3; DY = 4; % Geschwindigkeit
 BESTX = 5; BESTY = 6; % Beste individuelle Position
 PERF = 7; BESTPERF = 8; % 
 
-for run = 1:nrRuns
-%tic;
-nAuswertungen = 0;
-
 %Definitionsbereich der Zielfunktion
 minVal = -2*pi;
 maxVal = 2*pi;
 
 %Zielfunktion für Visualisierung erzeugen
-if plot
+if flagPlot
     x1 = linspace(minVal, maxVal, 100);
     x2 = linspace(minVal, maxVal, 100);
     y = zeros(length(x1), length(x1));
@@ -42,9 +36,18 @@ if plot
     end
 end
 
-%Schwarm anlegen und mit sinnvollen Werten initialisieren
+% 4) TESTLAEUFE
+% Zaehler fuer Iterationen und Funktionsaufrufe
+nIters = zeros (1, nrRuns);
+nFuncCalls = zeros (1, nrRuns);
+for run = 1:nrRuns
+    %tic;
+    nAuswertungen = 0;
+    nIter = 0;
+
+    % 4.1) INITIALISIERUNG
     schwarm = zeros(8,nrParticle);
-    
+
     %Startposition der Partikel
     if sPosRndElseRegl
         schwarm([X Y],:) = minVal + (maxVal-minVal) * rand(2,nrParticle);
@@ -53,7 +56,7 @@ end
             linspace(minVal, maxVal, sqrt(nrParticle)));
         schwarm([X Y],:) = [xMesh(:) yMesh(:)]';
     end
-    
+
     %Startgeschwindigkeit der Partikel
     if sVelRndElseZero
         schwarm([DX DY],:) = vmin + (vmax-vmin) * rand(2,nrParticle);
@@ -62,107 +65,106 @@ end
     else
         schwarm([DX DY],:) = zeros(2,nrParticle);
     end
-    
+
     %Bestes bisher gefundenes Ergebnis
     schwarm([BESTX BESTY],:) = schwarm([X Y],:);
-    
+
     %Performance der Partikel
     perf = f(schwarm([X Y],:));
     nAuswertungen = nAuswertungen + nrParticle;
     bestPerf = perf;
     schwarm([PERF BESTPERF],:) = [perf; bestPerf];
+    
+    % 4.2) SIMULATION
+    %Speicher fuer Verlaufsvariablen anlegen
+    winnerPerf = Inf;
+    lastWinnerPerf = Inf;
 
+    %Optimierungsschleife
     finished = false;
-    nIter = 0;
+    while ~finished
+        if flagPlot
+            %2D-Zielfunktion in den Hintergrund legen
+            pcolor(x2,x1,y), shading flat;
+            hold on
+            %und Schwarm darauf zeichnen
+            plot(schwarm(Y,:), schwarm(X,:), '.k');
+            %plot(schwarm(BESTY,:), schwarm(BESTX,:), 'ok');
+            hold off
+            drawnow;
+            pause(0.01);
+        end
 
-%Speicher fuer Verlaufsvariablen anlegen
-winnerPerf = Inf;
-lastWinnerPerf = Inf;
+        %alle Partikel bewerten
+        for j = 1:nrParticle
+            schwarm(PERF, j) = f(schwarm([X Y],j));
+            nAuswertungen = nAuswertungen + 1;
+        end
 
-%Optimierungsschleife
-while ~finished
-    if plot
-        %2D-Zielfunktion in den Hintergrund legen
-        pcolor(x2,x1,y), shading flat;
-        hold on
-        %und Schwarm darauf zeichnen
-        plot(schwarm(Y,:), schwarm(X,:), '.k');
-        %plot(schwarm(BESTY,:), schwarm(BESTX,:), 'ok');
-        hold off
-        drawnow;
-        pause(0.01);
+        %Besten des Schwarms ggf. merken und Abbruchbedingung pruefen
+        lastWinnerPerf = winnerPerf;
+        [winnerPerf,winnerIdx] = min(schwarm(PERF, :));
+        if (winnerPerf < 0.1 && abs(lastWinnerPerf - winnerPerf) < 0.1)
+            finished = true;
+        end
+
+        %Interaktion jedes Partikels auswerten
+        for j=1:nrParticle
+            % Individuelles Optimum aktualisieren
+            if schwarm(PERF, j) < schwarm(BESTPERF, j)
+                schwarm(BESTPERF, j) = schwarm(PERF, j);
+                schwarm([BESTX BESTY], j) = schwarm([X Y], j);
+            end
+
+            %Den Besten der beiden Nachbarn in der Matrix auswaehlen
+            leftNb = j-1; % linker Nachbar
+            if leftNb == 0
+                leftNb = nrParticle;
+            end
+            rightNb = j+1; % rechter Nachbar
+            if rightNb == nrParticle + 1
+                rightNb = 1;
+            end
+            if (schwarm(PERF, leftNb) < schwarm(PERF, rightNb))
+                bestNb = leftNb;
+            else
+                bestNb = rightNb;
+            end
+
+            %Eigene Bewegungsrichtung anpassen
+            x = schwarm([X Y], j);
+            xOpt = schwarm([BESTX BESTY], j);
+            xNb = schwarm([X Y], bestNb);
+            v = schwarm([DX DY], j);
+
+            phi1 = c1 * rand(2,1);
+            phi2 = c2 * rand(2,1);
+            v = c0*v + phi1 .* (xOpt - x) + phi2 .* (xNb - x);
+
+            %Geschwindigkeit auf [vmin, vmax] begrenzen
+            vNorm = norm(v);
+            if vNorm < vmin
+                v = (vmin/vNorm)*v;
+            end
+            if vNorm > vmax
+                v = (vmax/vNorm)*v;
+            end
+
+            %Bewegung ausfuehren
+            x = x + v;
+
+            %Ort und Geschwindigkeit speichern
+            schwarm([X Y], j) = x;
+            schwarm([DX DY], j) = v;
+        end
+        nIter = nIter + 1;
     end
-    
-    %alle Partikel bewerten
-    for j = 1:nrParticle
-        schwarm(PERF, j) = f(schwarm([X Y],j));
-        nAuswertungen = nAuswertungen + 1;
-    end
-    
-    %Besten des Schwarms ggf. merken und Abbruchbedingung pruefen
-    lastWinnerPerf = winnerPerf;
-    [winnerPerf,winnerIdx] = min(schwarm(PERF, :));
-    if (winnerPerf < 0.1 && abs(lastWinnerPerf - winnerPerf) < 0.1)
-        finished = true;
-    end
-    
-    %Verlaufsinformationen speichern
-    
-    %Interaktion jedes Partikels auswerten
-    for j=1:nrParticle
-        % Individuelles Optimum aktualisieren
-        if schwarm(PERF, j) < schwarm(BESTPERF, j)
-            schwarm(BESTPERF, j) = schwarm(PERF, j);
-            schwarm([BESTX BESTY], j) = schwarm([X Y], j);
-        end
-        
-        %Den Besten der beiden Nachbarn in der Matrix auswaehlen
-        leftNb = j-1; % linker Nachbar
-        if leftNb == 0
-            leftNb = nrParticle;
-        end
-        rightNb = j+1; % rechter Nachbar
-        if rightNb == nrParticle + 1
-            rightNb = 1;
-        end
-        if (schwarm(PERF, leftNb) < schwarm(PERF, rightNb))
-            bestNb = leftNb;
-        else
-            bestNb = rightNb;
-        end
-        
-        %Eigene Bewegungsrichtung anpassen
-        x = schwarm([X Y], j);
-        xOpt = schwarm([BESTX BESTY], j);
-        xNb = schwarm([X Y], bestNb);
-        v = schwarm([DX DY], j);
-        
-        phi1 = c1 * rand(2,1);
-        phi2 = c2 * rand(2,1);
-        v = c0*v + phi1 .* (xOpt - x) + phi2 .* (xNb - x);
-        
-        %Geschwindigkeit auf [vmin, vmax] begrenzen
-        vNorm = norm(v);
-        if vNorm < vmin
-            v = (vmin/vNorm)*v;
-        end
-        if vNorm > vmax
-            v = (vmax/vNorm)*v;
-        end
-        %Bewegung ausfuehren
-        x = x + v;
-        %Ort und Geschwindigkeit speichern
-        schwarm([X Y], j) = x;
-        schwarm([DX DY], j) = v;
-    end
-    nIter = nIter + 1;
+
+    %toc;
+    nIters(run)=nIter;
+    nFuncCalls(run)=nAuswertungen;
 end
 
-%Auswertung der Resultate
-%toc;
-nIters(run)=nIter;
-nFuncCalls(run)=nAuswertungen;
-end
-
+% 5) AUSWERTUNG DER RESULTATE
 disp(['Durchschnittliche Anzahl an Iterationen: ', num2str(mean(nIter))]);
 disp(['Durchschnittliche Anzahl an Auswertungen: ', num2str(mean(nFuncCalls))]);
